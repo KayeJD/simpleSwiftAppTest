979b63b053465dbe99b55f1d7312cac5a9f26750
Karryl Dumalag <139111295+KayeJD@users.noreply.github.com>
2025-01-19 16:58:18 -0700

    Initial Commit

---
+++ simpleTest/simpleTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  simpleTestApp.swift
+//  simpleTest
+//
+//  Created by Kaye on 1/19/25.
+//
+
+import SwiftUI
+
+@main
+struct simpleTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
