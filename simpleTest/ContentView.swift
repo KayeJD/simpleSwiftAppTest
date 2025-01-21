b9cbf68bf78fcf48ed9b7af33433e1af7b0eaa25
Karryl Dumalag <139111295+KayeJD@users.noreply.github.com>
2025-01-20 20:59:26 -0700

    Create simple greeting functionality

--- simpleTest/ContentView.swift
+++ simpleTest/ContentView.swift
@@ -10,38 +10,73 @@ import SwiftUI
 struct ContentView: View {
     @State private var firstName: String = ""
     @State private var lastName: String = ""
+    @State private var greetingMessage: String = ""
     
     var body: some View {
-        VStack(spacing: 10){
+        VStack(spacing: 20) {
             Text("Sample App")
                 .font(.largeTitle)
-            
             Text("Input your first and last name:")
         }
-            
-        Form {
+        
+        Form{
             Section{
                 TextField(
                     "First Name",
                     text: $firstName
-                )
+                ).disableAutocorrection(true)
                 TextField(
                     "Last Name",
                     text: $lastName
-                )
+                ).disableAutocorrection(true)
             }
-
+            
+            Section {
+                Text(greetingMessage)
+                    .font(.headline)
+                    .padding()
+            }
         }
-
-        Button(action: {}) {
-            Text("Show Message")
-              .padding()
-              .foregroundColor(.white)
-              .background(.pink)
-              .cornerRadius(10)
+        
+        Section {
+            HStack{
+                Button(action: {
+                    showGreeting()
+                }) {
+                    Text("Greeting")
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(Color.blue)
+                        .cornerRadius(10)
+                }.shadow(color: .blue, radius: 5, y: 5)
+                
+                Button(action: {
+                    resetFields()
+                }) {
+                    Text("Reset")
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(Color.gray)
+                        .cornerRadius(10)
+                }
+            }
         }
-        .shadow(color: .red, radius: 15, y: 5)
     }
+    
+    func resetFields() {
+        firstName = ""
+        lastName = ""
+        greetingMessage = ""
+    }
+    
+    func showGreeting() {
+        if firstName.isEmpty || lastName.isEmpty {
+            greetingMessage = "Please enter both your first and last name."
+        } else {
+            greetingMessage = "\(firstName) \(lastName) Welcome to CSE 335"
+        }
+    }
+
 }
 
 #Preview {
